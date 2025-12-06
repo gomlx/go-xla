7f1c8e5ab14df4e30ed1a999dd67c8696e9d8d9a
Jan Pfeifer <pfeifer@gmail.com>
2025-12-06 08:22:52 +0100

    Removed deprecated xlabuilder.

--- xlabuilder/xlabuilder_cpu_dynamic_test.go
+++
@@ -1,8 +0,0 @@
-//go:build pjrt_cpu_dynamic
-
-package xlabuilder_test
-
-import (
-	// Link CPU PJRT statically: slower but works on Mac.
-	_ "github.com/gomlx/gopjrt/pjrt/cpu/dynamic"
-)
--- xlabuilder/xlabuilder_cpu_static_test.go
+++
@@ -1,8 +0,0 @@
-//go:build pjrt_cpu_static
-
-package xlabuilder_test
-
-import (
-	// Link CPU PJRT statically: slower but works on Mac.
-	_ "github.com/gomlx/gopjrt/pjrt/cpu/static"
-)
